78e6f112f8d965e3842b28b017ee984e888c4ef0
Dorottya Hencsár <dorottya.hencsar@bitotron.com>
2022-05-18 13:39:15 +0200

    Person class

---
+++ demo/src/main/java/org/disz/demo/Asd.java
@@ -0,0 +1,6 @@
+package demo.src.main.java.org.disz.demo;
+
+import javax.persistence.*;
+
+public class Asd {
+}
---
+++ demo/src/main/java/org/disz/demo/Person.java
@@ -0,0 +1,68 @@
+package org.disz.demo;
+
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(
+            strategy= GenerationType.AUTO,
+            generator="native"
+    )
+    @GenericGenerator(
+            name = "native",
+            strategy = "native"
+    )
+    private Long id;
+
+    //protected long book.id;  =>  person-nak >=0 book.id-ja lehet
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected boolean isAdmin = false;
+    private String password;
+
+    protected Person(){}
+
+    public Person(String firstName, String lastName, String email, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    protected void changePassword(String oldPwd, String newPwd, String newPwd2){
+        if (oldPwd == this.password && newPwd == newPwd2){
+            this.password = newPwd;
+        }
+    }
+
+    private void setId(){
+
+    }
+}
+
